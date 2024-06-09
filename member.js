22291eacb72ab3665bf63996087834f5c2b89377
ana <ana.smirnova@outlook.com>
2024-06-09 12:22:21 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var member = {
+        name: 'John',
+        age: 20,
+        skills: ['JavaScript', 'React', 'Node', 'MongoDB'],
+        details: {
+            address: '123 Street',
+            city: 'New York'
+        }
+    };
+    return member;
+}
